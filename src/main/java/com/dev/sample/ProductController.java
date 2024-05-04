eacc5f8f92a2b1fca94bf326ce47f7941655953d
uday <devarakonda.uday@gmail.com>
2024-05-04 14:51:13 -0700

    test2

--- src/main/java/com/dev/sample/ProductController.java
+++ src/main/java/com/dev/sample/ProductController.java
@@ -14,7 +14,7 @@ public class ProductController {
 
     @GetMapping("/products")
     public List<NewProduct> getMethodName() {
-        System.out.println("git commit test and publish to central repo github, Lets go");
+        System.out.println("git commit test and publish to central repo github, Lets go! test");
         return Arrays.asList(
             new NewProduct(1,"Honda", "2024",45550.00),
             new NewProduct(2,"Honda CRV", "2024",45550.00));
